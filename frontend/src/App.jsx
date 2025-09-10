6c53c598258090d32f27d1d2c1d196f1f634f93c
KayanIrani <iranikayan5@gmail.com>
2025-09-10 11:24:53 +0530

    Changes in UI and a few modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,10 @@ import HelpLine from "./pages/HelpLine.jsx";
 import MeetDoc from "./pages/MeetDoc.jsx";
 import Chat from "./pages/Chat.jsx";
 import PptRender from "./pages/PptRender.jsx";
-
+import HomeStyles from './pages/css/HomeStyles.module.css';
 function App() {
   return (
-    <>
+    <div className={HomeStyles.mainContainer}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,7 +17,13 @@ function App() {
         <Route path="/helplines" element={<HelpLine />} />
         <Route path="/ppt" element={<PptRender />} />
       </Routes>
-    </>
+      <footer className="bg-dark text-center text-white py-3">
+        <p className="mb-0">
+          © 2025 Mindful Nutrition | About Us | Resources | Contact
+        </p>
+      </footer>
+
+    </div >
   );
 }
 
--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -2,7 +2,7 @@ import NavbarStyles from "./css/NavbarStyles.module.css";
 
 const Navbar = () => {
   return (
-    <nav id="navmenu" class={NavbarStyles.navmenu}>
+    <nav id="navmenu" className={NavbarStyles.navmenu}>
       <ul>
         <li>
           <a href="/">Home</a>
@@ -23,6 +23,10 @@ const Navbar = () => {
           <a href="/">Blogs</a>
         </li>
       </ul>
+      <div style={{width: '100%',display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div style={{ height: '1px',width: '80%',flex: 1, border: '1px solid gray'}}>
+        </div>
+      </div>
     </nav>
   );
 };
