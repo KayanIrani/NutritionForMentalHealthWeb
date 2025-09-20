8d1853ee7ac5e3b4a1175fa7e6e8b85c590d2d9a
KayanIrani <iranikayan5@gmail.com>
2025-09-20 17:35:30 +0530

    Helpline fine tuned

--- frontend/src/pages/HelpLine.jsx
+++ frontend/src/pages/HelpLine.jsx
@@ -4,7 +4,14 @@ import styles from './css/GlobalStyles.module.css'
 const HelpLine = () => {
   return (
     <div className={styles.tableContainer} >
-      <h1>HELPLINE NUMBERS FOR PwDs</h1>
+      {/* <h1>HELPLINE NUMBERS FOR PwDs</h1> */}
+      <h1>HELPLINES FOR EVERYONE'S WELL-BEING</h1>
+      <p>In times of emotional distress, psychological crisis, or mental health challenges, immediate support can make a life-saving difference.
+        <br />
+        The following helpline numbers have been compiled specifically for persons with individuals in need, offering assistance ranging from counseling and crisis intervention to suicide prevention and rehabilitation. These services are confidential, free of cost, and accessible across India. 
+        <br />
+        Whether you're seeking help for yourself or someone you care about, these helplines are here to listen and support without judgment.</p>
+        <p><a href="https://depwd.gov.in/en/others-helplines/" target='_blank' style={{textDecoration: 'none','color': '#4dcb64'}}>Credits</a></p>
       <table>
         <thead>
           <tr>
